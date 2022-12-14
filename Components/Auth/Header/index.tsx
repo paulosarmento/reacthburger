2408fa200a12a02aafdfa16ed56c66cacb2f8ea5
hugolrjdev <fortalezaplanodesaude@gmail.com>
2022-12-13 23:35:37 -0300

    style login

--- Components/Auth/Header/index.tsx
+++ Components/Auth/Header/index.tsx
@@ -1,18 +1,18 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
-
+import img from '../../../assets/images/logo-icon-orange.svg'
 const Header: NextPage = () => {
   return (
     <header>
       <Link href="/login">
         <a>
           <img
-            src="/images/logo-icon-red.svg"
+            src="/images/logo-icon-orange.svg"
             alt="Logo Icone"
             id="logo-icon"
           />
           <img
-            src="/images/logo-text-red.svg"
+            src="/images/logo-text-orange.svg"
             alt="Logo Texto"
             id="logo-text"
           />
